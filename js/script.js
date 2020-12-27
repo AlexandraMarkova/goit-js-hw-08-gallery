66e8089200dceaa9690dead77e267571b4cbf186
AlexandraMarkova <a.markova@ukr.net>
2020-12-28 00:52:32 +0200

    добавляет слушателя событий на картинки

--- js/script.js
+++ js/script.js
@@ -11,4 +11,21 @@ const imagesRef = items.map(
     (elem = `<li class="gallery__item"><a class="gallery__link" href="${elem.original}"><img class="gallery__image" src="${elem.preview}" data-source="${elem.original}"alt="${elem.description}"/></a></li>`)
 );
 
- galleryRef.insertAdjacentHTML("afterbegin", imagesRef.join(""));
+galleryRef.insertAdjacentHTML("afterbegin", imagesRef.join(""));
+ 
+galleryRef.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) { 
+    event.preventDefault();
+
+    console.log(event.target)
+
+    if (event.target.nodeName !== 'IMG') { 
+        return;
+    }
+    console.log('картинка');
+
+    const imageRef = event.target;
+
+    const largeImageURL = imageRef.dataset.source
+}
