8494616f4688cf289ee466113e174325e54cf4af
AlexandraMarkova <a.markova@ukr.net>
2020-12-28 17:24:17 +0200

    выполняет открытие модального окна по клику на элементе галереи

--- js/script.js
+++ js/script.js
@@ -12,20 +12,39 @@ const imagesRef = items.map(
 );
 
 galleryRef.insertAdjacentHTML("afterbegin", imagesRef.join(""));
+
  
 galleryRef.addEventListener('click', onGalleryClick);
 
 function onGalleryClick(event) { 
     event.preventDefault();
 
-    console.log(event.target)
-
-    if (event.target.nodeName !== 'IMG') { 
+    const imageRef = event.target;
+    console.log(event.target);
+    if (imageRef.nodeName !== 'IMG') { 
         return;
     }
-    console.log('картинка');
 
-    const imageRef = event.target;
+    const largeImageURL = imageRef.dataset.source;
+    console.log(largeImageURL);
 
-    const largeImageURL = imageRef.dataset.source
+console.log(imageRef.src);
+    imageRef.src = largeImageURL;
+    console.log(imageRef.src);
 }
+
+
+
+ const openModalImg = document.querySelector(".lightbox");
+
+ galleryRef.addEventListener("click", () => {
+   openModalImg.classList.add("is-open");
+ });
+
+
+// galleryRef.addEventListener('click', openModal);
+
+// function openModal(event) { 
+//   event.target;  
+// } 
+
